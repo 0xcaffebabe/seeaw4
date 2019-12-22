74fdd9d352a792c6ebf0087f343c7cbd1778c07c
MY <715711877@qq.com>
2019-12-22 22:43:16 +0800

    some change

--- seeaw4_android/app/src/main/java/com/example/seeaw4/seeaw4/android/MainActivity.java
+++ seeaw4_android/app/src/main/java/com/example/seeaw4/seeaw4/android/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.seeaw4.seeaw4.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TabHost;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
